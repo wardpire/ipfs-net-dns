7fba3251d2e711e30ad6ef3d99f600e429208af4
jdomnitz <380352+jdomnitz@users.noreply.github.com>
2024-07-09 01:05:59 -0400

    Correct AnswerAllQuestions documentation

--- src/Resolving/NameServer.cs
+++ src/Resolving/NameServer.cs
@@ -23,7 +23,7 @@ namespace Makaretu.Dns.Resolving
         /// </summary>
         /// <value>
         ///   <b>false</b> to answer <b>any</b> of the questions. 
-        ///   <b>false</b> to answer <b>all</b> of the questions.
+        ///   <b>true</b> to answer <b>all</b> of the questions.
         ///   The default is <b>false</b>.
         /// </value>
         /// <remarks>
